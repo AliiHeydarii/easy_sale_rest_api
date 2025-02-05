695efaaf6b93a522365a7333878b9491e574a7c5
Ali <heydari1375.ali@gmail.com>
2025-02-05 14:57:29 +0330

    add premission class to Adlist and AdDetial , add put and delete for AdDetail

--- api/views.py
+++ api/views.py
@@ -4,8 +4,13 @@ from .models import Advertisement
 from .serializers import AdListSerializer , AdDetailSerializer
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 
 class AdListApiView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
     def get(self, request):
         ad_list = Advertisement.objects.all()
         serializer = AdListSerializer(ad_list , many=True)
@@ -20,7 +25,22 @@ class AdListApiView(APIView):
     
 
 class AdDetailApiView(APIView):
+    permission_classes=[IsAuthenticatedOrReadOnly]
+    
     def get(self,request,pk):
         ad = get_object_or_404(Advertisement,pk=pk)
         serializer = AdDetailSerializer(ad)
         return Response(serializer.data)
+    
+    def put(self,request,pk):
+        ad = get_object_or_404(Advertisement,pk=pk)
+        serializer = AdDetailSerializer(ad , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+       ad = get_object_or_404(Advertisement,pk=pk)
+       ad.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
